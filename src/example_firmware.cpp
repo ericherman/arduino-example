553da608bb37192f05b02370d13919bba4a8cf03
Eric Herman <eric@freesa.org>
2013-12-08 17:00:57 +0100

    add LGPL

--- src/example_firmware.cpp
+++ src/example_firmware.cpp
@@ -1,6 +1,16 @@
 // modified from: http://www.windmeadow.com/node/38
 
-// See: http://arduino.cc/forum/index.php?topic=92364.0
+// Arduino firmware for a Serial "Rotate 13" service
+
+/*
+ Copyright (C) 2012,2013 Eric Herman <eric@freesa.org>
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+*/
+
 #include <Arduino.h>
 #include "rot13.h"
 
